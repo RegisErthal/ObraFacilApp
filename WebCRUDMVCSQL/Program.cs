8e74355db1ed7845add6860f957e52b7b88cc467
Régis Erthal <45245812+RegisErthal@users.noreply.github.com>
2023-05-10 22:10:32 -0300

    alter string connection

--- WebCRUDMVCSQL/Program.cs
+++ WebCRUDMVCSQL/Program.cs
@@ -4,16 +4,27 @@ using ObraFacilApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+        .AddJsonFile("appsettings.localhost.json", optional: true)
+        .AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var Configuration = builder.Configuration;
+
 builder.Services.AddDbContext<Contexto>
     (options => options.UseSqlServer
-    ("Data Source=ACERNITRO;Initial Catalog=master;User ID=sa;Password=root; trustServerCertificate=true"));
+    (Configuration.GetConnectionString("master")));
+    //("Data Source=ACERNITRO;Initial Catalog=master;User ID=sa;Password=root; trustServerCertificate=true"));
     //SEnha banco Azure:
     //ObraFacil2023 servidor: acernitro
     //("Server = tcp:bancoobrafacil.database.windows.net, 1433; Initial Catalog = ObraFacil - banco; Persist Security Info=False; User ID =acernitro; Password ={ObraFacil2023}; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30"));
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
