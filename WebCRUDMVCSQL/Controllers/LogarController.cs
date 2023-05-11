c64de0a4b17758fb7f94688ed077fdb7e24313f8
Régis Erthal <45245812+RegisErthal@users.noreply.github.com>
2023-05-10 23:11:16 -0300

    validaemail

--- WebCRUDMVCSQL/Controllers/LogarController.cs
+++ WebCRUDMVCSQL/Controllers/LogarController.cs
@@ -1,11 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ObraFacilApp.Models;
 
 namespace ObraFacilApp.Controllers
 {
     public class LogarController : Controller
     {
+        private readonly Contexto _context;
+
+        public LogarController(Contexto context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Logar dadosLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                if (dadosLogin == null || _context.Login == null)
+                    return NotFound();
+
+                var login = await _context.Login
+                    .FirstOrDefaultAsync(m => m.UserName == dadosLogin.UserName
+                                              && m.Senha == dadosLogin.Senha);
+
+                if (login == null)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return LocalRedirect("/Home");
+            }
             return View();
         }
     }
---
+++ WebCRUDMVCSQL/Models/Logar.cs
@@ -0,0 +1,11 @@
+﻿namespace ObraFacilApp.Models
+{
+    public class Logar
+    {
+        public string UserName { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
+
+
