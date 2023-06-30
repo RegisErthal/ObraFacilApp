fb7bccd7eb33101a396686a17db279b5a3202feb
Aduan Nadalon Vieira <aduannv@gmail.com>
2023-06-29 23:11:46 -0300

    adjusts

--- WebCRUDMVCSQL/Controllers/EtapasController.cs
+++ WebCRUDMVCSQL/Controllers/EtapasController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using ObraFacilApp.Models;
 
 namespace ObraFacilApp.Controllers
@@ -15,12 +16,14 @@ namespace ObraFacilApp.Controllers
 
         public IActionResult Index(int id)
         {
+            var session = HttpContext.Session.GetString("ObraFacilUsuario");
+            var usuario = JsonConvert.DeserializeObject<LoginModel>(session);
+
             var fundacao = _context.Fundacao?.FirstOrDefault(m => m.ProjetoId == id);
             var alvenaria = _context.Alvenaria?.FirstOrDefault(m => m.ProjetoId == id);
             var cobertura = _context.Cobertura?.FirstOrDefault(m => m.ProjetoId == id);
             var eletrica = _context.Eletrica?.FirstOrDefault(m => m.ProjetoId == id);
             var hidraulica = _context.Hidraulica?.FirstOrDefault(m => m.ProjetoId == id);
-
 
             decimal percFundacaoOk = QtdOkFundacao(fundacao);
             decimal percAlvenariaOk = QtdOkAlvenaria(alvenaria);
@@ -30,7 +33,22 @@ namespace ObraFacilApp.Controllers
 
             var model = new EtapasModel();
             model.Id = id;
+
             model.FundacaoPerc = percFundacaoOk;
+            model.AlvenariaPerc = percFundacaoOk;
+            model.CoberturaPerc = percFundacaoOk;
+            model.EletricaPerc = percFundacaoOk;
+            model.HidraulicaPerc = percFundacaoOk;
+
+            model.TemFundacao = fundacao != null;
+            model.TemAlvenaria = alvenaria != null;
+            model.TemCobertura = cobertura != null;
+            model.TemEletrica = eletrica != null;
+            model.TemHidraulica = hidraulica != null;
+
+            model.MostraCronograma = model.TemFundacao && model.TemAlvenaria && model.TemCobertura && model.TemEletrica && model.TemHidraulica;
+            model.UsuarioIsAdmin = usuario.isAdmin;
+
             return View(model);
         }
 
