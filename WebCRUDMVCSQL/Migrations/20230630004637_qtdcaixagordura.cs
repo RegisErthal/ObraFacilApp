08b4f2385fb547f700c851af9dabe0bb12bfc6d5
Régis Erthal <45245812+RegisErthal@users.noreply.github.com>
2023-06-29 22:03:33 -0300

    MetragemQuadrada

---
+++ WebCRUDMVCSQL/Migrations/20230630004637_qtdcaixagordura.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ObraFacilApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class qtdcaixagordura : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QtdCaixaGortduraOk",
+                table: "Hidraulica",
+                newName: "QtdCaixaGorduraOk");
+
+            migrationBuilder.RenameColumn(
+                name: "QtdCaixaGortdura",
+                table: "Hidraulica",
+                newName: "QtdCaixaGordura");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QtdCaixaGorduraOk",
+                table: "Hidraulica",
+                newName: "QtdCaixaGortduraOk");
+
+            migrationBuilder.RenameColumn(
+                name: "QtdCaixaGordura",
+                table: "Hidraulica",
+                newName: "QtdCaixaGortdura");
+        }
+    }
+}
