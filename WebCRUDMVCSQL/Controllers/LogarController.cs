01f8ab0817030e1ae52f4d8e1de646f9e5ab7932
Régis Erthal <45245812+RegisErthal@users.noreply.github.com>
2023-08-16 00:04:10 -0300

    Add validacao de senha incorreta e criptografia de senha de iususario

--- WebCRUDMVCSQL/Controllers/LogarController.cs
+++ WebCRUDMVCSQL/Controllers/LogarController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using ObraFacilApp.Models;
+using System.Threading.Tasks;
 
 namespace ObraFacilApp.Controllers
 {
@@ -9,40 +10,32 @@ namespace ObraFacilApp.Controllers
     {
         private readonly ContextoModel _context;
 
-        public LogarController(ContextoModel context)
-        {
+        public LogarController(ContextoModel context) {
             _context = context;
         }
 
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LogarModel dadosLogin)
-        {
-            if (ModelState.IsValid)
-            {
-                if (dadosLogin == null || _context.Login == null)
-                    return NotFound();
-
+        [ValidateAntiForgeryToken] // Adiciona proteção CSRF
+        public async Task<IActionResult> Login(LogarModel dadosLogin) {
+            if (ModelState.IsValid) {
                 var login = await _context.Login
                     .FirstOrDefaultAsync(m => m.UserName == dadosLogin.UserName
                                               && m.Senha == dadosLogin.Senha);
 
-                if (login == null)
-                {
-                    ModelState.AddModelError("", "Usuário ou senha incorretos."); // Adiciona mensagem de erro à ModelState
-                    return View(); // Retorna a view com a mensagem de erro
-                }
-                else
-                {
+                if (login == null) {
+                    ModelState.AddModelError("", "Usuário ou senha incorretos.");
+                    return View("Index", dadosLogin); // Retorna para a view com os dados de login e a mensagem de erro
+                } else {
                     HttpContext.Session.SetString("ObraFacilUsuario", JsonConvert.SerializeObject(login));
                     return LocalRedirect("/Home");
                 }
             }
-            return View();
+
+            return View("Index", dadosLogin); // Retorna para a view com os dados de login
         }
     }
 }
